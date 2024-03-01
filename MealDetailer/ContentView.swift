8553b1dbd20bbafda997e05c2eccc0a650904f8d
Atharva Pashankar <atharva24@gmail.com>
2024-02-29 23:50:58 -0800

    Initial Commit

---
+++ MealDetailer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealDetailer
+//
+//  Created by Atharva Pashankar on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MealDetailer/MealDetailerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDetailerApp.swift
+//  MealDetailer
+//
+//  Created by Atharva Pashankar on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MealDetailerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
